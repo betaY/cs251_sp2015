7ad0bc6ab966212cb100deab87129d7e7303fc96
Junyu <beta168921@gmail.com>
2015-01-16 21:25:05 -0500

    cs251 spring2015 project0

--- 251Spring15P0/MaxFind.java
+++ 251Spring15P0/MaxFind.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class MaxFind {
 	private static int getMaxVal(int[] arr) {
 		/* TODO: Find the maximum integer in 'arr' and return it */
+		int max = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+
+		return max;
 	}
 	
 	public static void main(String args[]) {
